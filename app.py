8cf8de17fc2ba7e9e8b99a0923cd01bf48dacc86
Joe Cheng <joe@posit.co>
2024-05-25 14:40:02 -0700

    Production-ready version of the app

--- app.py
+++ app.py
@@ -1,53 +1,44 @@
-import base64
-import tempfile
+from shiny import reactive, req
+from shiny.express import input, render, ui, session
+from shinymedia import input_video_clip, audio_spinner
+from query import chat
 
-from openai import AsyncOpenAI
-from shiny.express import input, render, ui
+input_video_clip(
+    "clip", reset_on_record=True, class_="mt-5 mx-auto", style="max-width: 600px;"
+)
 
-from videoinput import audio_spinner, input_video_clip, process_video
-
-client = AsyncOpenAI()
-
-ui.page_opts(class_="py-5")
-
-input_video_clip("clip")
+messages = []
 
 
-@render.ui
-async def show_clip():
-    clip = input.clip()
-    mime_type = clip["type"]
-    bytes = base64.b64decode(clip["bytes"])
-    # TODO: Use correct file extension based on mime type
-    with tempfile.TemporaryDirectory() as tempdir:
-        filename = tempfile.mktemp(dir=tempdir, suffix=get_video_extension(mime_type))
-        with open(filename, "wb") as file:
-            file.write(bytes)
-            file.close()
-
-        with ui.Progress() as p:
-
-            mp3_data_uri = await process_video(
-                client,
-                filename,
-                callback=lambda status: p.set(message=status),
-            )
-            return audio_spinner(src=mp3_data_uri)
+@reactive.extended_task
+async def chat_task(video_clip, messages, session):
+    with ui.Progress(session=session) as p:
+        return await chat(video_clip, messages, p)
 
 
-def get_video_extension(mime_type: str) -> str:
-    mime_type = mime_type.split(";")[0].strip()
+@reactive.effect
+@reactive.event(input.clip, ignore_none=False)
+def start_chat():
+    chat_task.cancel()
+    req(input.clip())
+    chat_task(input.clip(), messages, session)
 
-    # Dictionary to map MIME types to file extensions
-    mime_to_extension = {
-        "video/webm": ".webm",
-        "video/mp4": ".mp4",
-        "video/ogg": ".ogv",
-        "video/x-matroska": ".mkv",
-        "video/avi": ".avi",
-        "video/mpeg": ".mpeg",
-        "video/quicktime": ".mov",
-    }
 
-    # Return the appropriate file extension for the given MIME type
-    return mime_to_extension.get(mime_type, "")
+# Show the chat response
+@render.express
+def response():
+    if chat_task.status() == "initial":
+        ui.p(
+            ui.strong("Instructions:"),
+            " Record a short video clip to start chatting.",
+            class_="mt-4 text-center",
+        )
+        return
+
+    with ui.hold():
+        req(input.clip())
+
+    if chat_task.status() == "running":
+        req(False)
+
+    audio_spinner(src=chat_task.result())
--- videoinput/__init__.py
+++
@@ -1,8 +0,0 @@
-from .ui import input_video_clip, audio_spinner
-from .query import process_video
-
-__all__ = (
-    "input_video_clip",
-    "audio_spinner",
-    "process_video",
-)
